2d6a1ae08c99f1a18188885b02f4c0ae0990fd0b
김용선 <yongson.kim@vntgcorp.com>
2021-06-25 16:01:28 +0900

    CHG: 설정파일 값 샘플 보기 적용

--- welcome/views.py
+++ welcome/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
+from decouple import config
 
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'welcome/hello_world.html')
+    db_info = {
+        'name': config('POSTGRES_NAME'),
+        'user': config('POSTGRES_USER'),
+    }
+    context = {'test_data': db_info}
+    return render(request, 'welcome/hello_world.html', context)
