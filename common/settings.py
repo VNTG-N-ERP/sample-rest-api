00cd564068cf015a0f18fe0a46fc7f235d49ea2b
김용선 <yongson.kim@vntgcorp.com>
2021-06-28 09:05:50 +0900

    CHG: static 설정 변경

--- common/settings.py
+++ common/settings.py
@@ -140,3 +140,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
